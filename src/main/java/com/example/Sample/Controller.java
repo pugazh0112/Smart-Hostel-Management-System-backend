d4c2451cf63305362052d7fa712ac3972763ade7
pugazh0112 <vigneshpugal2@gmail.com>
2026-03-25 19:01:58 +0530

    Added CORS

--- src/main/java/com/example/Sample/Controller.java
+++ src/main/java/com/example/Sample/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 public class Controller {
 
